47443dd0af4eafae6f2fe67cb61bc5fefa883ab9
Raul <jimenezraul1981@gmail.com>
2023-01-21 00:37:58 -0500

    added auth class with a function to check for auth, generate token, add credentials and to validate  token

---
+++ test/index.js
@@ -0,0 +1,28 @@
+const Auth = require('@jimenezraul/auth');
+
+const auth = new Auth('1m', '1d', ['http://localhost:3000']);
+
+console.log(auth.accessExp); // return the expiration time for access and refresh tokens
+console.log(auth.refreshExp); // return the expiration time for access and refresh tokens
+console.log(auth.allowedOrigins); // return an array of allowed origins
+
+const accessToken = auth.generateToken(
+  { firstName: 'John', lastName: 'Doe' },
+  'accessToken'
+);
+
+console.log(accessToken); // return a valid access token
+
+const refreshToken = auth.generateToken(
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+  },
+  'refreshToken'
+);
+
+console.log(refreshToken); // return a valid refresh token
+
+console.log(auth.validateToken(refreshToken)); // return true if token is valid
+
+module.exports = auth;
